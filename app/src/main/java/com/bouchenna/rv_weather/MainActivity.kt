e123d8c9f2e1276c515bdd0f43f91c8e5b439375
versus10-prog <150893635+versus10-prog@users.noreply.github.com>
2024-03-26 12:38:21 +0100

    Changement style fragment home

--- app/src/main/java/com/bouchenna/rv_weather/MainActivity.kt
+++ app/src/main/java/com/bouchenna/rv_weather/MainActivity.kt
@@ -1,44 +1,58 @@
 package com.bouchenna.rv_weather
 
+
 import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
-import android.widget.Button
+import android.view.Menu
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.bouchenna.rv_weather.databinding.ActivityMainBinding
+import com.sonney.valentin.LocalisationAdapter
+import android.widget.Button
+import android.widget.ImageView
 import androidx.core.view.GravityCompat
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bouchenna.rv_weather.databinding.ActivityMainBinding
 import com.bouchenna.rv_weather.service.FireBase_db
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.sonney.valentin.LocalisationAdapter
 import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private lateinit var user: FirebaseAuth
+    private lateinit var menuBurger: ImageView
+    private lateinit var deconnexion: Button
+    private lateinit var firebaseDb: FireBase_db
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: LocalisationAdapter
     private lateinit var addLocalisation: Button
-    private lateinit var firebaseDb: FireBase_db
-    private lateinit var deletedLocalisation: Localisation
-    private var deletedPosition: Int = -1
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+    private var locs: ArrayList<Localisation> = ArrayList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        user = FirebaseAuth.getInstance()
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navController = navHostFragment.navController
+        menuBurger = binding.menuBurgerImageView
+        deconnexion = binding.menuCustomInclude.header.buttonDeconnexion
         firebaseDb = FireBase_db()
+        user  = FirebaseAuth.getInstance()
         recyclerView = binding.menuCustomInclude.recyclerViewMenu
         recyclerView.layoutManager = LinearLayoutManager(this)
         adapter = LocalisationAdapter(this)
@@ -46,44 +60,78 @@ class MainActivity : AppCompatActivity() {
         addLocalisation = binding.menuCustomInclude.addLocButton
         getData()
 
-        addLocalisation.setOnClickListener {
-            findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.nav_home)
+        user.currentUser?.email?.let {
+            binding.menuCustomInclude.header.textViewUserName.text = it
         }
+
+        deconnexion.setOnClickListener{
+
+            Firebase.auth.signOut()
+            val intent = Intent(this, ConnexionActivity::class.java)
+            startActivity(intent)
+        }
+
+        menuBurger.setOnClickListener{
+            val drawerLayout: DrawerLayout = binding.drawerLayout
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        addLocalisation.setOnClickListener{
+            navController.navigate(R.id.nav_home)
+        }
+
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
 
-    fun suppression(loc: Localisation): Boolean {
-        lifecycleScope.launch {
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun suppression(loc: Localisation): Boolean{
+
+        lifecycleScope.launch{
             firebaseDb.deleteLocalisation(loc)
             getData()
         }
-        return true
+        return  true
     }
 
-    fun showMmeteo(loc: Localisation): Boolean {
+    fun showMmeteo(loc: Localisation): Boolean{
+
+        var long = loc.coord!!.longitude
+        var lat = loc.coord!!.latitude
+
         val bundle = Bundle().apply {
             putString("nom", loc.nom)
-            loc.coord?.let { putDouble("long", it.longitude) }
-            loc.coord?.let { putDouble("lat", it.latitude) }
-        }
-        findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.action_nav_home_to_nav_gallery, bundle)
-        return true
+            putDouble("long", long )
+            putDouble("lat", lat)}
+        navController.navigate(R.id.action_nav_home_to_nav_gallery, bundle)
+
+        return  true
     }
 
-    private fun getData() {
+    fun getData (){
         lifecycleScope.launch {
-            val locs = firebaseDb.getLocalisations(user.uid.toString())
+            locs = firebaseDb.getLocalisations(user.uid.toString())
             adapter.submitList(locs)
+            adapter.notifyDataSetChanged()
         }
     }
 
-    fun addData(loc: Localisation): Boolean {
-        lifecycleScope.launch {
+    fun addData (loc: Localisation): Boolean{
+        lifecycleScope.launch{
             loc.userId = user.uid.toString()
             firebaseDb.addLocalisation(loc)
             getData()
         }
         return true
     }
+
 }
